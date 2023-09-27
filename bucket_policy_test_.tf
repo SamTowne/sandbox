1c274b506972ce28c2794cdc6e3b4c3025f9d734
Sam Towne <sam.towne2@gmail.com>
2023-09-27 08:43:31 -0700

    destroy sandbox infra

--- bucket_policy_test_.tf
+++
@@ -1,4 +0,0 @@
-module "bucket_policy_test" {
-  source        = "./modules/bucket_policy_test"
-  module_prefix = "test-bucket-${data.aws_caller_identity.current.id}"
-}
---
+++ sandbox.tf
@@ -0,0 +1,6 @@
+# Destroyed 9/27/2023
+
+# module "bucket_policy_test" {
+#   source        = "./modules/bucket_policy_test"
+#   module_prefix = "test-bucket-${data.aws_caller_identity.current.id}"
+# }
